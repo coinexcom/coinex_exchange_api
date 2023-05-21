c32a21dd4b07b9ee9bfcc822b8a87296ef8329b0
Mola Pahnadayan <mola.mp@gmail.com>
2023-05-21 15:48:35 +0330

    Small bug fix

--- qt/main.cpp
+++ qt/main.cpp
@@ -6,7 +6,7 @@ int  main(int argc, char *argv[])
 {
     QCoreApplication  a(argc, argv);
 
-    CoinexAPI *coinexAPI = new CoinexAPI("ACCESS_TOKEN", "SECRET_TOKEN", this);
+    CoinexAPI *coinexAPI = new CoinexAPI("ACCESS_TOKEN", "SECRET_TOKEN");
     coinexAPI->start();
 
     return a.exec();
